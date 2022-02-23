a3b5456d617c6b50849f12b9867bb69d0531d0b9
Gustavo Marques <gustavo.markez23@gmail.com>
2022-02-23 19:35:24 -0300

    second commit

--- Conta.js
+++ Conta.js
@@ -1,29 +1,26 @@
 export class Conta{
-    constructor(saldoInicial, cliente, agencia) {
-        this._saldo = saldoInicial;
-        this._cliente = cliente;
-        this._agencia = agencia;
+  constructor(saldoInicial, cliente, agencia) {
+    this._saldo = saldoInicial;
+    this._cliente = cliente;
+    this._agencia = agencia;
+  }
+
+  sacar(valor) {
+    if (this._saldo >= valor) {
+      this._saldo -= valor;
+      return valor;
     }
+  }
 
-    sacar(valor) {
-        taxa = 1.1 * valor;
-        if (this._saldo >= valor) {
-            this._saldo -= valor;
-            return valor;
-        }
+  depositar(valor) {
+    if (valor <= 100) {
+      return;
     }
+    this._saldo += valor;
+  }
 
-    depositar(valor) {
-        if (valor <= 100) {
-            return;
-        }
-        this._saldo += valor;
-    }
-
-    tranferir(valor, conta) {
-
-        const valorSacado = this.sacar(valor);
-        conta.depositar(valorSacado);
-
-    }
+  tranferir(valor, conta) {
+    const valorSacado = this.sacar(valor);
+    conta.depositar(valorSacado);
+  }
 }
--- ContaCorrente.js
+++ ContaCorrente.js
@@ -1,55 +1,19 @@
-import {
-    Cliente
-} from "./Cliente.js";
+import { Conta } from "./Conta.js";
 
-export class ContaCorrente {
-    static numeroDeContas = 0;
+export class ContaCorrente extends Conta{
+  static numeroDeContas = 0;
 
-    // #saldo =0 https://github.com/tc39/proposal-class-fields#private-fields
+  constructor(cliente, agencia) {
+    super(0, cliente, agencia);
+    ContaCorrente.numeroDeContas += 1;
+  }
 
-
-
-
-    set cliente(novoValor) {
-        if (novoValor instanceof Cliente) {
-            this._cliente = novoValor;
-        }
+  sacar(valor){
+    let taxa = 1.1;
+    const valorSacado = taxa * valor;
+    if(this._saldo >= valorSacado){
+      this._saldo -= valorSacado;
+      return valorSacado;
     }
-
-    get cliente() {
-        return this._cliente;
-    }
-
-    get saldo() {
-        return this._saldo;
-    }
-
-    constructor(agencia, cliente) {
-        this.agencia = agencia;
-        this.cliente = cliente;
-        this._saldo = 0;
-        ContaCorrente.numeroDeContas += 1;
-    }
-
-
-    sacar(valor) {
-        if (this._saldo >= valor) {
-            this._saldo -= valor;
-            return valor;
-        }
-    }
-
-    depositar(valor) {
-        if (valor <= 100) {
-            return;
-        }
-        this._saldo += valor;
-    }
-
-    tranferir(valor, conta) {
-
-        const valorSacado = this.sacar(valor);
-        conta.depositar(valorSacado);
-
-    }
+  }
 }
