e3fdb061faa9775945a1c1083d70362b09c53d3f
Gustavo Marques <gustavo.markez23@gmail.com>
2022-02-25 13:23:40 -0300

    finalizando abstração

--- Conta.js
+++ Conta.js
@@ -1,15 +1,25 @@
+//Classe abstrata
 export class Conta{
   constructor(saldoInicial, cliente, agencia) {
+    if(this.constructor == Conta)
+      throw new Error("Você não deveria instanciar um objeto do tipo Conta");
     this._saldo = saldoInicial;
     this._cliente = cliente;
     this._agencia = agencia;
   }
 
+  //Metodo abstrato
   sacar(valor) {
+    throw new Error('O método sacar da conta é abstrato');
+  }
+
+  _sacar(valor, taxa){
+    const valorSacado = taxa * valor;
     if (this._saldo >= valor) {
       this._saldo -= valor;
       return valor;
     }
+    return 0;
   }
 
   depositar(valor) {
--- ContaCorrente.js
+++ ContaCorrente.js
@@ -8,12 +8,9 @@ export class ContaCorrente extends Conta{
     ContaCorrente.numeroDeContas += 1;
   }
 
+  //SOBREESCREVENDO
   sacar(valor){
     let taxa = 1.1;
-    const valorSacado = taxa * valor;
-    if(this._saldo >= valorSacado){
-      this._saldo -= valorSacado;
-      return valorSacado;
-    }
+    return this._sacar(valor, taxa);
   }
 }
